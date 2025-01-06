f0ee4062f3c4f0b52934b73a9c94ddbf4ae6fac7
Sander <sanderderidder@hotmail.nl>
2025-01-06 12:05:05 +0100

    setup directive better

--- docs/source/_extensions/ib_element_directive.py
+++ docs/source/_extensions/ib_element_directive.py
@@ -9,9 +9,9 @@ from docutils.parsers.rst import Directive
 
 from sphinx.application import Sphinx
 from sphinx.ext.autodoc import ClassDocumenter, PropertyDocumenter
-from sphinx.util import inspect as sphinx_inspect, docstrings
+from sphinx.util import inspect as sphinx_inspect, docstrings, logging
 from PythonScreenStackManager import elements
-from PythonScreenStackManager.elements.baseelements import Element
+from PythonScreenStackManager.elements.baseelements import Element, colorproperty, elementaction
 
 def get_parent_elements(element_class: type[Element]):
     parent_classes = list(element_class.__bases__)
@@ -22,20 +22,31 @@ def get_parent_elements(element_class: type[Element]):
             if cls != Element:
                 parent_classes.extend()
 
+_LOGGER = logging.getLogger(__name__)
 
 def create_default_dict(element_class: type[Element]):
 
-    base_args = inspect.signature(element_class.__init__)
-    mro_classes = list(inspect.getmro(elements.Tile))   #0 is class itself, than highest is older parent
+    #0 is class itself, than highest is older parent
     ##This can be nested relatively ok by recursively calling the function.
     ##make dict: start at class itself, create dict. Then continue and call setdefault
-    f = mro_classes[0].__elt_init__ ##use this as the original __init__ is overwritten by __init_subclass__
-    base_args = inspect.signature(f)
+    
+    # f = mro_classes[0].__elt_init__ ##use this as the original __init__ is overwritten by __init_subclass__
+    
+    if element_class == Element:
+        init_func = Element.__init__
+    else:
+        init_func = element_class.__init__
+    
+    base_args = inspect.signature(init_func)
     required_args = []
     optional_args = {}
+
+    mro_classes = list(inspect.getmro(element_class)) 
     for param in base_args.parameters.values():
         if param.default == param.empty:
-            if param.name != "self": required_args.append(param.name)
+            if param.name == "self" or param.kind == param.VAR_KEYWORD or param.kind == param.VAR_POSITIONAL:
+                continue 
+            required_args.append(param.name)
         else:
             optional_args[param.name] = param.default
     for parent_cls in mro_classes[1:]:
@@ -57,17 +68,17 @@ def create_default_dict(element_class: type[Element]):
 class inkBoardElement(ClassDocumenter):
 
     objtype = '-inkboardelement'
+    option_spec = ClassDocumenter.option_spec | {"summary-docstr": lambda *arg: True}
 
     def get_object_members(self, want_all):
         members = super().get_object_members(True)
         ##Seem to not get the properties from parents. Gotta fix that.
-        elt_cls = getattr(self.module,self.object_name)
-        self.element_class = elt_cls
-        required_args, optional_args = create_default_dict(elt_cls)
-        elt_cls.element_required_args = required_args
-        elt_cls.element_optional_args = optional_args
+
+        required_args, optional_args = create_default_dict(self.element_class)
+        self.element_class.element_required_args = required_args
+        self.element_class.element_optional_args = optional_args
         property_members = []
-        docced_props = set()
+        found_required = set()
         for mem in members[1]:
             # if mem.docstring:
                 ##For properties: the docstring is not yet present.
@@ -75,42 +86,13 @@ class inkBoardElement(ClassDocumenter):
             if sphinx_inspect.isproperty(mem.object):
                 
                 prop_name = mem.object.fget.__name__
-                docstr = mem.object.__doc__
-
-                if prop_name in docced_props:
-                    continue
-
-                if not docstr: 
-                    docstr = ""
-                else:
-                    docstr = docstr.strip()
-                    if "\n" in docstr:
-                        docstrs = docstr.splitlines()
-                        docstrs = [string.strip() for string in docstrs]
-                        docstr = " ".join(docstrs)
-                    if not docstr.endswith((".","?","!", ",",";")):
-                        docstr = docstr + "."
-
-                ##docstring is changed for each property has they get doubled oops.
-                ##So changing the docstrings in place is not ideal perhaps
-                ##Maybe do so in the sorter? as it has the documenter. Or just set the objects docstring?
                 if prop_name in optional_args:
                     if mem.object.fset == None:
                         continue
-                    default_val = optional_args[prop_name]
-                    if isinstance(default_val, str):
-                        docstr = f"""| {docstr}
-                        | **Optional**, defaults to ``"{default_val}"``
-                        """
-                    else:
-                        ##For dicts, see if they can be converted to yaml?
-                        docstr = f"""| {docstr}
-                        | **Optional**, defaults to ``{default_val}``
-                        """
-                    # mem.object.__doc__ = docstr
+                    pass
                 elif prop_name in required_args:
-                    docstr = f"""**Required**. {docstr}"""
-                    # mem.object.__doc__ = docstr
+                    found_required.add(prop_name)
+                    pass
                 else:
                     continue
 
@@ -123,28 +105,54 @@ class inkBoardElement(ClassDocumenter):
                 # print(mem.object)
 
         ##Don't forget to sort them too. But also, check if that possible happens again after this function returns.
+        for prop in required_args:
+            if prop not in found_required and prop != "kwargs":
+                _LOGGER.warning(f"{elt_cls}: Missing required argument {prop} in documentation")
         return False, property_members
 
     def sort_members(self, documenters, order):
-        for documenter, _ in documenters:
+        # sorted_docs = super().sort_members(documenters, order)
+        color_props = []
+        action_props = []
+        other_props = []
+        
+        for documenter_tuple in documenters:
+            documenter = documenter_tuple[0]
             documenter.options.noindex = True
+            prop_name = documenter.name.split(".")[-1]
+            prop = getattr(self.element_class,prop_name)
+            if isinstance(prop,colorproperty):
+                color_props.append(documenter_tuple)
+            elif isinstance(prop,elementaction):
+                action_props.append(documenter_tuple)
+            else:
+                other_props.append(documenter_tuple)
             ##To edit the docstrings: need an elementproperty documenter.
             ##Simply based on propertydocumenter, just overwrite the get_doc function.
             ##Or maybe just append something to the end of it before returning it.
-        return super().sort_members(documenters, order)
+        
+        ##On top: the results of action_shorthands etc? or put those into the element docstrings maybe
+        color_props.sort(key=lambda e: e[0].name)
+        action_props.sort(key=lambda e: e[0].name)
+        other_props.sort(key=lambda e: e[0].name)
+
+        sorted_docs = [*color_props, *action_props, *other_props]
+        return sorted_docs
 
     def generate(self, more_content = None, real_modname = None, check_module = False, all_members = False):
 
         ##generate in the parent class simply does this too.
         ##But in here simply setup the stuff to be documented.
         old_name = self.name
-        new_name = f"PythonScreenStackManager.elements.baseelements.{self.name}"
+        if self.name == "Element":
+            modname = "PythonScreenStackManager.elements.baseelements"
+        else:
+            modname = "PythonScreenStackManager.elements"
+
+        new_name = f"{modname}.{self.name}"
         self.name = new_name
         self.objtype = 'class'
         self.options.setdefault("inherited-members", {'object'})
-
-        # self.options.setdefault("no-index", True)
-        # self.options.no_index = True
 
         ##docstr edit: simply find the paramaters line and split from there?
         ##Should be roughly safe.
@@ -153,23 +161,41 @@ class inkBoardElement(ClassDocumenter):
         ##action_shorthands, color_properties
         ##See if the emulator icons can be included somehow? but maybe for a later point
         ##Same with automatically generating images probably.
-        ##no-index
         ##all properties that are settable
-        ##How to deal with defaults though?
 
         r = super().generate(more_content, real_modname, check_module, all_members)
-        # self.element_class = getattr(self.module, old_name)
-        ##Contents from parsing: put into attribute directive.result.data. Is in strings.
-        ##Use that to parse the defaults into the properties (use the function inspector function I wrote for devices to get the defaults.)
-        ##See self.document_members: If *all_members* is True, document all members, else those given by
-        # *self.options.members*.
-        ##Maybe overwrite get_object_members?
-        ##And check if the docstring can be gathered from there
 
         ##For testing this:
         ##Add the stuff for the ClassDocumenter and see how it is parsed.
         ##From there: figure out what properties to put in.
         return r
+
+
+    def get_doc(self):
+        original_docstr = super().get_doc()
+        elt_cls = getattr(self.module,self.object_name)
+        self.element_class: Element = elt_cls
+
+        if self.options.get("summary-docstr", False):
+            new_str = original_docstr[0][0]
+            short_doc = [new_str]
+        else:
+            short_doc = []
+            for docstr in original_docstr[0]:
+                if docstr.strip() == "Parameters":
+                    break
+                short_doc.append(docstr)
+
+        short_doc.append(" ")
+        short_doc.append("Available shorthand actions are:")
+        self.fullname
+        for action, function in self.element_class.action_shorthands.items():
+            d = docstrings.prepare_docstring(inspect.getdoc(getattr(self.element_class, function)))
+            func_summary = d[0]
+            short_doc.append(f' - ``{action}``: {func_summary}')
+
+        short_doc.append(" ")
+        return [short_doc]
 
 class ElementPropertyDocumenter(PropertyDocumenter):
     ##Set the thingy for priority one higher
